6a0d61775db92e9d21e11299a617c7ee306d7f8c
Samantha Malone <sammac99@gmail.com>
2021-08-04 11:15:23 -0700

    added model associations

--- models/index.js
+++ models/index.js
@@ -1,1 +1,34 @@
+const User = require("./User");
+const Post = require("./Post");
+const Comment = require("./Comment");
 
+// create model associations
+User.hasMany(Post, {
+  foreignKey: "user_id",
+});
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+});
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "cascade",
+  hooks: true,
+});
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+  onDelete: "cascade",
+  hooks: true,
+});
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+  onDelete: "cascade",
+  hooks: true,
+});
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+  onDelete: "cascade",
+  hooks: true,
+});
+
+// export modules
+module.exports = { User, Post, Comment };
