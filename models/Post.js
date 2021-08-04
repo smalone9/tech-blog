88d4aa3140b3e0ed44dd33bc4097011b2d149cc2
Samantha Malone <sammac99@gmail.com>
2021-08-04 11:01:24 -0700

    added Post model

---
+++ models/Post.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+// Create Post model
+class Post extends Model {}
+
+// set up Post fields/columns
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    post_url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isURL: true,
+      },
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      reference: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "post",
+  }
+);
+
+// export Post model
+module.exports = Post;
