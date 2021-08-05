bfd861b04a2f207078d19f8c6194cedaa4f09a2b
Samantha Malone <sammac99@gmail.com>
2021-08-05 12:36:12 -0700

    added serevr connection to rotes and db

---
+++ server.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const express = require("express");
+const exphbs = require("express-handlebars");
+const helpers = require("utils/helpers");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require("./config/connection");
+
+// set up handlebars
+const hbs = exphbs.create({ helpers });
+
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const sess = {
+  secret: "top secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+// set up Handlebars template engine
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+// turn on routes
+app.use(require("./controllers/"));
+
+// turn on server and db connection
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
+});
