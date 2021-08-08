7c2b7abf3a03aebe4cf85348e4104422ee79e924
Samantha Malone <sammac99@gmail.com>
2021-08-08 16:57:44 -0700

    added comment seeds

---
+++ seeds/comment-seeds.js
@@ -0,0 +1,39 @@
+const { Comment } = require("../models");
+
+const commentData = [
+  {
+    comment_text: "Have a great day!",
+    user_id: 2,
+    post_id: 1,
+  },
+  {
+    comment_text: "Fantastic foxes",
+    user_id: 4,
+    post_id: 7,
+  },
+  {
+    comment_text: "Rockstars",
+    user_id: 2,
+    post_id: 5,
+  },
+  {
+    comment_text: "Grape Jelly",
+    user_id: 3,
+    post_id: 3,
+  },
+  {
+    comment_text: "Have you heard?",
+    user_id: 2,
+    post_id: 1,
+  },
+  {
+    comment_text: "Have a great day!",
+    user_id: 9,
+    post_id: 10,
+  },
+  {
+    comment_text: "Tokyo Olympics",
+    user_id: 2,
+    post_id: 11,
+  },
+];
