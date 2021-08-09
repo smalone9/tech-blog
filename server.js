4745ce63331c048b6ad779d03f2307943274e1eb
Samantha Malone <sammac99@gmail.com>
2021-08-08 17:24:29 -0700

    refactored server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const express = require("express");
 const exphbs = require("express-handlebars");
-const helpers = require("utils/helpers");
+const helpers = require("./utils/helpers");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
