fc01ab70d96c1d9bc816bf4d6a996e1bb11ff65d
Samantha Malone <sammac99@gmail.com>
2021-08-10 13:00:27 -0700

    debugging connection

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,34 +1,75 @@
 const router = require("express").Router();
+const sequelize = require("../config/connection");
 const { Post, User, Comment } = require("../models");
 const withAuth = require("../utils/auth");
 
-router.get("/dashboard", withAuth, async (req, res) => {
-  try {
-    const allPost = await Post.findAll({});
-    const posts = allPost.map((post) => post.get({ plain: true }));
-    res.render("dashboard", { posts });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
-
-router.get("/edit/:id", withAuth, async (req, res) => {
-  try {
-    const onePost = await Post.findByPk({
-      where: { id: req.params.id },
-      include: {
+router.get("/", withAuth, (req, res) => {
+  console.log(req.session);
+  console.log("==================");
+  Post.findAll({
+    where: {
+      user_id: req.session.user_id,
+    },
+    attributes: ["id", "post_url", "title", "created_at"],
+    include: [
+      {
         model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
       },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("dashboard", { posts, loggedIn: true });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
-    const post = onePost.get({ plain: true });
-    res.render("single-post", post);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
 });
 
-router.get("/create-post", (req, res) => {});
+router.get("/edit/:id", withAuth, (req, res) => {
+  Post.findByPk(req.params.id, {
+    attributes: ["id", "post_url", "title", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (dbPostData) {
+        const post = dbPostData.get({ plain: true });
+
+        res.render("edit-post", {
+          post,
+          loggedIn: true,
+        });
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
