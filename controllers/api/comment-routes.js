3eb08d59d495c07f4a39aef3c28df1e1ed48fb7e
Samantha Malone <sammac99@gmail.com>
2021-08-10 12:34:18 -0700

    debugging routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -2,49 +2,45 @@ const router = require("express").Router();
 const { Comment } = require("../../models");
 const withAuth = require("../../utils/auth");
 
-router.get("/", async (req, res) => {
-  try {
-    const allComment = await Comment.findAll({});
-    const comments = allComment.map((comment) => comment.get({ plain: true }));
-    res.render("comments", { comments });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
+router.get("/", (req, res) => {
+  console.log("===================");
+  Comment.findAll()
+    .then((dbCommentData) => res.json(dbCommentData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-router.post("/", withAuth, async (req, res) => {
-  try {
-    const oneComment = await Comment.create({
-      comment_text: req.body.comment_text,
-      user_id: req.session.user_id,
-      post_id: req.body.post_id,
+router.post("/", withAuth, (req, res) => {
+  Comment.create({
+    comment_text: req.body.comment_text,
+    user_id: req.session.user_id,
+    post_id: req.body.post_id,
+  })
+    .then((dbCommentData) => res.json(dbCommentData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
-    const oneComment = oneComment.map((comment) =>
-      comment.comment({ plain: true })
-    );
-    res.render("new-comment", { comments });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
 });
 
-router.delete("/:id", withAuth, async (req, res) => {
-  try {
-    const oneComment = await Comment.destroy({
-      where: {
-        id: req.params.id,
-      },
+router.delete("/:id", withAuth, (req, res) => {
+  Comment.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbCommentData) => {
+      if (!dbCommentData) {
+        res.status(404).json({ message: "No comment matching this ID" });
+        return;
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
-    const oneComment = oneComment.map((comment) =>
-      comment.delete({ plain: true })
-    );
-    res.render("delete-comment", { comments });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
 });
 
 module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,46 +1,87 @@
 const router = require("express").Router();
+const sequelize = require("../config/connection");
 const { Post, User, Comment } = require("../models");
-const withAuth = require("../../utils/auth");
 
 router.get("/login", (req, res) => {
   res.render("login");
 });
 
-router.get("/", withAuth, async (req, res) => {
-  try {
-    const allPost = await Post.findAll({});
-    const posts = allPost.map((post) => post.get({ plain: true }));
-    res.render("homepage", { posts });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
-
-router.get("/post/:id", withAuth, async (req, res) => {
-  try {
-    const onePost = await Post.findOne({
-      where: { id: req.params.id },
-      include: [
-        {
-          model: Comment,
-          include: {
-            model: User,
-            attributes: ["username"],
-          },
-        },
-        {
+router.get("/", (req, res) => {
+  console.log(req.session);
+  Post.findAll({
+    attributes: ["id", "post_url", "title", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
           model: User,
           attributes: ["username"],
         },
-      ],
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("homepage", {
+        posts,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
-    const post = onePost.get({ plain: true });
-    res.render("single-post", post);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
+});
+
+router.get("/post/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "post_url", "title", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ plain: true });
+        return;
+      }
+      const post = dbPostData.get({ plain: true });
+      res.render("single-post", {
+        post,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
   }
+  res.render("login");
 });
 
 module.exports = router;
